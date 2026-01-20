081f091c878728b662fe431c023ccc9236dbfa48
Johannes Weigel <johannes@weigel.dev>
2026-01-20 15:33:46 +0100

    fix(preprocessing): load env file in init

--- src/avasimrt/__init__.py
+++ src/avasimrt/__init__.py
@@ -1,5 +1,12 @@
+from pathlib import Path
+from dotenv import load_dotenv
+
 from .app import run
 from .config import SimConfig
 from .result import SimResult
+
+_env_file = Path.cwd() / ".env"
+if _env_file.exists():
+    load_dotenv(_env_file)
 
 __all__ = ["run", "SimConfig", "SimResult"]
